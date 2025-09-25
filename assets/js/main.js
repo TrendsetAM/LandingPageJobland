115ed760efda2d70dd6c631bcf90f749c5e4396e
TrendsetAM <trendsetam01@gmail.com>
2025-09-25 20:05:39 +0200

    new logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,9 @@
 
     // === logo change
     if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/an-icon-of-a-bot-head.png";
+      logo.src = "assets/images/logo/RDLogoB.png";
     } else {
-      logo.src = "assets/images/logo/an-icon-of-a-bot-head.png";
+      logo.src = "assets/images/logo/RDLogoC.png";
     }
 
     // show or hide the back-top-top button
